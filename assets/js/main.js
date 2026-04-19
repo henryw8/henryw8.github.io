cfef3c51a172fe26a2e93ef2215e3928d37c86d2
henryw8 <wangh491@gmail.com>
2026-04-19 16:42:47 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,8 +49,8 @@
                 }
             });
         }, {
-            threshold: 0.1,
-            rootMargin: '0px 0px -50px 0px'
+            threshold: 0,
+            rootMargin: '0px 0px 50px 0px'
         });
 
         elements.forEach(function (el) {
